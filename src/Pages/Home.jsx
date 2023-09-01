78a301327befb021c40652994697bd53c2b92cce
Taha Shah <tahashah411@gmail.com>
2023-09-01 23:17:43 +0500

    redirect issue

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,23 @@
+import { useEffect } from "react"
 import BoardView from "../Components/BoardView"
+import { useNavigate } from "react-router-dom"
 
 export default function Home() {
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        window.addEventListener('beforeunload', () => {
+          localStorage.setItem('prevURL', JSON.stringify(window.location))
+        })
+        const prevURL = JSON.parse(localStorage.getItem('prevURL'))
+
+        if (prevURL) {
+            const path = prevURL.pathname
+            localStorage.removeItem('prevURL')
+            navigate(`${path}`)
+        }
+      }, [])
     
     return (<BoardView delay={20000}/>)
 }
