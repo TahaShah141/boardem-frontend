ced8f2526efe04d1b7e7fb9d922eb77989b7fc63
Taha Shah <tahashah411@gmail.com>
2023-09-01 19:58:14 +0500

    change title tags

--- src/Pages/Boards/BoardLink.jsx
+++ src/Pages/Boards/BoardLink.jsx
@@ -32,7 +32,7 @@ export default function BoardLink({board, allowed=true}) {
             <p className={`text-white text-xl text-center py-2 bg-neutral-800 rounded-xl font-mono border-4 border-black tracking-widest w-full`}>{board.name}</p>
             <div className="flex self-center justify-center bg-neutral-950 border-black border gap-1 w-48 rounded-b-lg">
                 <div className="text-white w-5 h-5">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>lock</title><path d="M12,17A2,2 0 0,0 14,15C14,13.89 13.1,13 12,13A2,2 0 0,0 10,15A2,2 0 0,0 12,17M18,8A2,2 0 0,1 20,10V20A2,2 0 0,1 18,22H6A2,2 0 0,1 4,20V10C4,8.89 4.9,8 6,8H7V6A5,5 0 0,1 12,1A5,5 0 0,1 17,6V8H18M12,3A3,3 0 0,0 9,6V8H15V6A3,3 0 0,0 12,3Z" fill="currentColor"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>locked</title><path d="M12,17A2,2 0 0,0 14,15C14,13.89 13.1,13 12,13A2,2 0 0,0 10,15A2,2 0 0,0 12,17M18,8A2,2 0 0,1 20,10V20A2,2 0 0,1 18,22H6A2,2 0 0,1 4,20V10C4,8.89 4.9,8 6,8H7V6A5,5 0 0,1 12,1A5,5 0 0,1 17,6V8H18M12,3A3,3 0 0,0 9,6V8H15V6A3,3 0 0,0 12,3Z" fill="currentColor"/></svg>
                 </div>
                 {board.owner && <p className="inline font-mono tracking-widest">{board.owner}</p>}
             </div>
